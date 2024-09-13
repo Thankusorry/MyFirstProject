1b5bd8e1873fdb5d5840fbf7e27b1924f1c558d7
Thankusorry <1312934470@qq.com>
2024-09-13 23:27:19 +0800

    删掉重复，新增第三章内容

---
+++ Java代码/Java code/src/java代码/运算符/逻辑运算符.java
@@ -0,0 +1,13 @@
+package java代码.运算符;
+
+public class 逻辑运算符 {
+    public static void main(String[] args) {
+        int a=10,b=20;
+        if(a++>100 && b++ <100) {
+            System.out.println("& is good");
+        }
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
