9badceba830d07d9f19598c69889c626dc5b4809
Thankusorry <1312934470@qq.com>
2024-09-29 21:33:31 +0800

    2024/9/29

--- Java代码/Java code/src/java代码/运算符/逻辑运算符.java
+++ Java代码/Java code/src/java代码/运算符/逻辑运算符.java
@@ -1,13 +1,22 @@
-package java代码.运算符;
-
-public class 逻辑运算符 {
-    public static void main(String[] args) {
-        int a=10,b=20;
-        if(a++>100 && b++ <100) {
-            System.out.println("& is good");
-        }
-        System.out.println(a);
-        System.out.println(b);
-
-    }
-}
+//package java代码.运算符;
+//class C {
+//    private int p;
+//
+//    public C() {
+//        System.out.println("C()");
+//        this(0);
+//    }
+//    public C(int p) {
+//        p=p;
+//    }
+//    public void setP(int p) {
+//        p = p;
+//    }
+//}
+//
+//public class 逻辑运算符 {
+//    public static void main(String[] args) {
+//
+//
+//    }
+//}
